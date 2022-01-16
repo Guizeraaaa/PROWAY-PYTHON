0443953a93fd7e461ba53649c26a1f7488ae3367
Guilherme Correia <92228735+Guizeraaaa@users.noreply.github.com>
2022-01-16 16:15:03 -0300

    Add files via upload

---
+++ EX01.py
@@ -0,0 +1,11 @@
+nota01 = int(input("Digite a nota 1: "))
+nota02 = int(input("Digite a nota 2: "))
+nota03 = int(input("Digite a nota 3: "))
+media = (nota01+nota02+nota03) /3
+nome = input("Digite seu nome")
+sobrenome = input("Informe seu sobrenome")
+idade = input("Informe a sua idade")
+
+print("seunome é: "+nome+""+sobrenome"" + idade) 
+print("A sua média foi de : ",media)
+
---
+++ ex02.py
@@ -0,0 +1,6 @@
+rua = input("Digite a sua rua: ")
+bairro = input("Digite o seu bairro: ")
+rua = input("Digite o seu cep: ")
+cidade = input("Digite a sua cidade: ")
+
+print("A sua rua é", rua)
