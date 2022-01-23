c9eb240457c3040673603b50a46de4ffa6a11e53
Guilherme Correia <92228735+Guizeraaaa@users.noreply.github.com>
2022-01-23 18:55:11 -0300

    Add files via upload

---
+++ Aula01.py
@@ -0,0 +1,23 @@
+#texto = "Ola mundo"
+
+#numero = 1
+#numero02 = 1.5
+
+#print("Olá Alunos")
+
+#nome = input("Digite seu nome")
+
+#print(nome)
+
+#toda variavel alimentada por input é string 
+#Interações com pytho
+
+#ano = input(" Digite o ano que nasceu: ")
+
+#idade = 2022-int(ano)
+
+#print(idade)
+
+idade = int(input("Digite sua idade"))
+
+if i
